6db693515516a2e293bacf1e70fb0d177f18d251
KidmI2929 <kdm6068@gmail.com>
2026-09-23 16:11:10 +0900

    Bump v0.5.1 crash fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kidmi.videotowebp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.5.1"
 
         ndk {
             abiFilters += "arm64-v8a"
