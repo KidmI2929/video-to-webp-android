0b74375e0b43097cf857e0027129e963089dfd4d
KidmI2929 <kdm6068@gmail.com>
2026-09-23 16:52:29 +0900

    Bump v0.6.1 split fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kidmi.videotowebp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.6.1"
 
         ndk {
             abiFilters += "arm64-v8a"
