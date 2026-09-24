217b22ba6c781c5be1af9565ebb10edcccd2cfb9
KidmI2929 <kdm6068@gmail.com>
2026-09-24 09:06:17 +0900

    Bump Motion WebP 1.2.1 stability build [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kidmi.videotowebp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.2.0"
+        versionCode = 15
+        versionName = "1.2.1"
 
         ndk {
             abiFilters += "arm64-v8a"
